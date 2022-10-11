d1ecf2d507261594d8b60845460aeafdc5a8488c
unknown <sajeda1605akter@gmail.com>
2022-10-10 17:52:26 -0700

    header part complete

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,15 +1,19 @@
 import React, { useContext } from 'react';
 import { QuizContext } from '../../Main/Main';
 import Quiz from '../Quiz/Quiz';
+import img from '../../image/1.jpg'
 
 const Home = () => {
     const quizs=useContext(QuizContext);
     console.log(quizs.data)
     return (
         <section style={{width:'960px' ,margin:'auto'}}>
-            <div className=''>
-                <div className='container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 text-gray-900'>
-                    <h1>Welcome</h1>
+           
+                <div className='mb-24 '>
+                    <h1 className='text-4xl mb-12 text-center font-bold'>Welcome to our Quiz World</h1>
+                    <div className='flex items-center border border-abmer-100 rounded'>
+                        <img src={img} alt="" className='w-full rounded mr-6 p-2' />
+                    <p className='text-xl'>A quiz is a form of game or mind sport in which players attempt to answer questions correctly about a certain or variety of subjects. Quizzes can be used as a brief assessment in education and similar fields to measure growth in knowledge, abilities, or skills.In an educational context, a quiz is usually a form of a student assessment.</p></div>
                 </div>
                 <div className='grid grid-cols-3 md:grid-cols-2  '>
                 {
@@ -17,7 +21,7 @@ const Home = () => {
                 } 
                 </div>
 
-            </div>
+        
         </section>
     );
 };
